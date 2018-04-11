22362ed4216b393b5c62a050f5533863849ed7fa
Doug Valenta <dougvalenta@gmail.com>
2018-04-11 06:49:55 -0700

    #1: First pass at inscribable doc comments; correcting type parameterization of inscribe method

--- src/main/java/net/dougvalenta/scriptorium/Inscribable.java
+++ src/main/java/net/dougvalenta/scriptorium/Inscribable.java
@@ -11,21 +11,50 @@ import java.io.IOException;
 import java.util.Optional;
 
 /**
+ * Provides support for polyglot inscription services and inversion of flow control.
+ * 
+ * <p>
  * Implementers may choose to pass another type {@code <I>} to consumers, e.g. a
- * broader type that does not have methods consumers should not call.
+ * broader type that does not have methods consumers should not call (like close() or then()).
+ * This is referred to as the inscribed type.
  * 
  * @author Doug Valenta
- * @param <I> the type to pass to consumers
+ * @param <I> the inscribed type
  * @param <THIS> this type
  */
 public interface Inscribable<I, THIS extends Inscribable<I, THIS>> {
 	
-	public default <T extends FluentNode<THIS>> T inscribe(IOFunction<? super THIS, ? extends T> function) throws IOException {
+	public default <T extends FluentNode<THIS>> T inscribe(IOFunction<? super THIS, T> function) throws IOException {
 		return function.apply((THIS) this);
 	}
 	
+	/**
+	 * Calls the provided {@link IOConsumer}, passing an instance of the inscribed
+	 * type, and returns this object.
+	 * 
+	 * <p>
+	 * When this method returns, the provided consumer's {@link IOConsumer#accept(Object)}
+	 * method will have already returned.
+	 * 
+	 * @param consumer the {@link IOConsumer} to call with an instance of the inscribed type
+	 * @return this object
+	 * @throws IOException if an I/O error occurs 
+	 */
 	public THIS with(IOConsumer<? super I> consumer) throws IOException;
 	
+	/**
+	 * Calls the provided {@link IOBiConsumer}, passing the provided element and an 
+	 * instance of the inscribed type, and returns this object.
+	 * 
+	 * <p>
+	 * When this method returns, the provided bi-consumer's {@link IOBiConsumer#accept(Object, Object)}
+	 * method will have already returned.
+	 * 
+	 * @param element the element to pass as the first argument of the provided bi-consumer
+	 * @param biConsumer the {@link IOBiConsumer} to call with an instance of the inscribed type
+	 * @return this object
+	 * @throws IOException if an I/O error occurs 
+	 */
 	public <T> THIS with(T element, IOBiConsumer<? super T, ? super I> biConsumer) throws IOException;
 	
 	public default THIS withIf(final boolean condition, final IOConsumer<? super I> consumer) throws IOException {
