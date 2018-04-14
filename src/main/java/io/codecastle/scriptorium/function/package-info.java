591525a5c39ad7fdf5979652958018855cc9e8b1
Doug Valenta <dougvalenta@gmail.com>
2018-04-14 08:26:59 -0700

    Adding additional details for function package doc comment

--- src/main/java/io/codecastle/scriptorium/function/package-info.java
+++ src/main/java/io/codecastle/scriptorium/function/package-info.java
@@ -5,5 +5,10 @@
 
 /**
  * General-purpose functional interfaces for I/O-centric operations.
+ * 
+ * <p>
+ * The difference between interfaces in this package and similarly-named interfaces in
+ * {@link java.util.function} is that implementors of interfaces in this package may throw
+ * {@link java.io.IOException}.
  */
 package io.codecastle.scriptorium.function;
